742f6e2789e6043d27d2b68baba82f086d9ecf8b
Camille Scholtz <camille@airmail.cc>
2020-03-29 00:56:59 +0100

    Remove old code

--- popup.go
+++ popup.go
@@ -82,59 +82,6 @@ func (bar *Bar) popup(key string) *Popup {
 	return i.(*Popup)
 }
 
-/*func initPopup(x, y, w, h int, bg, fg string) (*Popup, error) {
-	popup := new(Popup)
-	var err error
-
-	// Create a window for the bar. This window listens to button press events
-	// in order to respond to them.
-	popup.win, err = xwindow.Generate(X)
-	if err != nil {
-		return nil, err
-	}
-	popup.win.Create(X.RootWin(), x, y, w, h, xproto.CwBackPixel|xproto.
-		CwEventMask, 0x000000, xproto.EventMaskButtonPress)
-
-	// EWMH stuff.
-	if err := initEWMH(popup.win.Id); err != nil {
-		return nil, err
-	}
-
-	// Map window.
-	popup.win.Map()
-
-	// XXX: Moving the window is again a hack to keep OpenBox happy.
-	popup.win.Move(x, y)
-
-	// Create the bar image.
-	popup.img = xgraphics.New(X, image.Rect(0, 0, w, h))
-	if err := popup.img.XSurfaceSet(popup.win.Id); err != nil {
-		panic(err)
-	}
-	popup.img.XDraw()
-
-	popup.w = w
-	popup.h = h
-
-	popup.bg = bg
-	popup.fg = fg
-
-	popup.drawer = &font.Drawer{
-		Dst:  popup.img,
-		Face: face,
-	}
-
-	// Color the background.
-	popup.img.For(func(cx, cy int) xgraphics.BGRA {
-		return popup.bg
-	})
-
-	// Draw the popup.
-	popup.draw()
-
-	return popup, nil
-}*/
-
 func (popup *Popup) draw() {
 	popup.img.XDraw()
 	popup.img.XPaint(popup.win.Id)
